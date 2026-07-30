edd60b1999868702a20550a03fbbe0f7ea721c0b
Aditya Shukla <adityashukla0227@gmail.com>
2026-07-30 17:00:17 +0530

    Ultron v0.4 - Added Memory Recall System

--- core/conversation.py
+++ core/conversation.py
@@ -1,3 +1,6 @@
+from core.memory import get_memory
+
+
 def handle_conversation(user):
     if user == "hi":
         print("Ultron: Hello!")
@@ -6,5 +9,23 @@ def handle_conversation(user):
     elif user == "how are you":
         print("Ultron: I am working perfectly!")
         return True
+
+    elif user.startswith("do you remember"):
+        memories = get_memory()
+
+        if memories:
+            for memory in memories:
+                if memory in user:
+                    print(f"Ultron: Yes, I remember {memory}.")
+                    return True
+
+            print("Ultron: I remember these things:")
+            for memory in memories:
+                print(f"- {memory}")
+            return True
+
+        else:
+            print("Ultron: I don't remember anything yet.")
+            return True
 
     return False
--- core/memory.py
+++ core/memory.py
@@ -1,5 +1,15 @@
 MEMORY_FILE = "data/memory.txt"
 
+
 def save_memory(text):
     with open(MEMORY_FILE, "a") as file:
         file.write(text + "\n")
+
+
+def get_memory():
+    try:
+        with open(MEMORY_FILE, "r") as file:
+            memories = file.readlines()
+            return [memory.strip() for memory in memories]
+    except FileNotFoundError:
+        return []
